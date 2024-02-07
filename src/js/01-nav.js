2bcdc3a29f912794c618f191886ac56f92a586ed
Dan Allen <dan@opendevise.com>
2024-02-07 03:48:10 -0700

    make nav script resilient if nav container is missing from page

--- src/js/01-nav.js
+++ src/js/01-nav.js
@@ -4,6 +4,7 @@
   var SECT_CLASS_RX = /^sect(\d)$/
 
   var navContainer = document.querySelector('.nav-container')
+  if (!navContainer) return
   var navToggle = document.querySelector('.nav-toggle')
   var nav = navContainer.querySelector('.nav')
   var navMenuToggle = navContainer.querySelector('.nav-menu-toggle')
